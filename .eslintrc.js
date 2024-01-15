ad87b0e6187ee1ab151776915fb0523c9776fda2
hiim207 <20jin_lee@naver.com>
2024-01-15 15:22:54 +0900

    asdfasdf

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,7 @@ module.exports = {
         "react"
     ],
     "rules": {
-	"jsx-a11y/alt-text": "off"
+	"jsx-a11y/alt-text": "off",
 	"no-console": "error",
     	"import/prefer-default-export": "off"
     }
